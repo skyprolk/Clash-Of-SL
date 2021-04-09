48d515c1eb1914cb47fb502dde914a1273caf93b
KNOiT <67490509+knoit-sky@users.noreply.github.com>
2021-04-09 19:43:19 +0530

    Add files via upload

---
+++ Clash SL Server [UCS 7.4.0]/Database/Redis.cs
@@ -0,0 +1,34 @@
+﻿namespace CSS.Database
+{
+    using StackExchange.Redis;
+    using CSS.Core;
+    using CSS.Helpers;
+    using Database = Logic.Enums.Database;
+
+    internal class Redis
+    {
+        internal static IDatabase Players;
+        internal static IDatabase Clans;
+        internal static IDatabase ClanWars;
+        internal static IDatabase Battles;
+
+        internal Redis()
+        {
+            ConfigurationOptions Configuration = new ConfigurationOptions();
+
+            Configuration.EndPoints.Add(Utils.ParseConfigString("RedisIPAddress"), Utils.ParseConfigInt("RedisPort"));
+
+            Configuration.Password = Utils.ParseConfigString("RedisPassword");
+            Configuration.ClientName = this.GetType().Assembly.FullName;
+
+            ConnectionMultiplexer Connection = ConnectionMultiplexer.Connect(Configuration);
+
+            Redis.Players  = Connection.GetDatabase((int) Database.Players);
+            Redis.Clans    = Connection.GetDatabase((int) Database.Clans);
+            Redis.ClanWars = Connection.GetDatabase((int)Database.ClanWars);
+            Redis.Battles  = Connection.GetDatabase((int)Database.Battles);
+
+            Logger.Say("Redis Database has been succesfully loaded.");
+        }
+    }
+}
